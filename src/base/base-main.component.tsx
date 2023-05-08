98c9e357449582ba7f8bc1d9b1d6f5dad646d7c4
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-05-08 13:58:22 +0700

    code improvisation

--- src/base/base-main.component.tsx
+++ src/base/base-main.component.tsx
@@ -1,64 +1,62 @@
-import React from 'react';
-import './base-layout.css';
-import { Layout, Menu } from 'antd';
-import { MenuList } from '../drawer-menu-list';
+import React from "react";
+import "./base-layout.css";
+import { Layout, Menu } from "antd";
+import { MenuList } from "../drawer-menu-list";
 import { useNavigate } from "react-router-dom";
 
 const { Sider, Content } = Layout;
 const { SubMenu } = Menu;
 
-export const BaseMainComponent = (
-  props: { children: JSX.Element | React.ReactNode }
-) => {
-
+export const BaseMainComponent = ({
+  children,
+}: {
+  children: JSX.Element | React.ReactNode;
+}) => {
   const navigate = useNavigate();
   function onClickChangeChildren(page: any) {
     navigate(page);
   }
 
   return (
-    <Layout className='first-layout'>
+    <Layout className="first-layout">
       <Sider trigger={null}>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={[MenuList[0].label]}>
-          {
-            MenuList.map((value) => {
-              if (value.hasSubs && value.subs && value.subs.length > 0) {
-                let submenu = value.subs.map((sub) => (
-                  <Menu.Item
-                    key={`${value.path}${sub.path}`}
-                    onClick={() => onClickChangeChildren(`${value.path}${sub.path}`)}
-                  >
-                    {sub.label}
-                  </Menu.Item>
-                ))
-                return (
-                  <SubMenu key={value.label} title={value.label}>
-                    {submenu}
-                  </SubMenu>
-                )
-              }
-              else {
-                return (
-                  <Menu.Item
-                    key={value.path}
-                    onClick={() => onClickChangeChildren(value.path)}
-                  >
-                    {value.label}
-                  </Menu.Item>
-                )
-              }
-            })
-          }
+          defaultSelectedKeys={[MenuList[0].label]}
+        >
+          {MenuList.map((value) => {
+            if (value.hasSubs && value.subs && value.subs.length > 0) {
+              return (
+                <SubMenu key={value.label} title={value.label}>
+                  {value.subs.map((sub) => (
+                    <Menu.Item
+                      key={`${value.path}${sub.path}`}
+                      onClick={() =>
+                        onClickChangeChildren(`${value.path}${sub.path}`)
+                      }
+                    >
+                      {sub.label}
+                    </Menu.Item>
+                  ))}
+                </SubMenu>
+              );
+            } else {
+              return (
+                <Menu.Item
+                  key={value.path}
+                  onClick={() => onClickChangeChildren(value.path)}
+                >
+                  {value.label}
+                </Menu.Item>
+              );
+            }
+          })}
         </Menu>
       </Sider>
       <Layout className="second-layout">
-        <Content className="content">
-          {props.children}
-        </Content>
+        <Content className="content">{children}</Content>
       </Layout>
     </Layout>
   );
-}
+};
